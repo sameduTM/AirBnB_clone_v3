b74f496924fe8b8112079babf52c321d424a6e26
Ken Wekesa <wanyamak884@gmail.com>
2025-01-20 13:28:59 +0300

    set content type to json

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """index file for views"""
 from api.v1.views import app_views
-import json
+from flask import jsonify, json, current_app
 
 @app_views.route('/status')
 def status():
-	status_obj = {"status": "OK"} 
-	return json.dumps(status_obj, indent=2)
+	o_dct = {"status": "OK"}
+
+	return current_app.response_class(json.dumps(o_dct, indent=2),
+		mimetype="application/json")
