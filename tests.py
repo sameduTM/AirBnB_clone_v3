7f6bd0a7d6ae64351414ec955f49031e8c121acc
Ken Wekesa <wanyamak884@gmail.com>
2025-01-23 19:20:40 +0300

    Correct output for get with FileStorage and Object existing

--- tests.py
+++ tests.py
@@ -5,13 +5,10 @@ import json
 import requests
 
 if __name__ == "__main__":
-    """ Get one state
+    """ POST /api/v1/states/
     """
-    r = requests.get("http://0.0.0.0:5000/api/v1/states")
-    r_j = r.json()
-    state_id = r_j[0].get('id')
-
-    """ PUT /api/v1/states/<state_id>
-    """
-    r = requests.put("http://0.0.0.0:5000/api/v1/states/{}".format(state_id), data={ 'name': "NewStateName" }, headers={ 'Content-Type': "application/x-www-form-urlencoded" })
+    r = requests.post("http://0.0.0.0:5000/api/v1/states/", data=json.dumps({ 'name': "NewState" }), headers={ 'Content-Type': "application/json" })
     print(r.status_code)
+    r_j = r.json()
+    print(r_j.get('id') is None)
+    print(r_j.get('name') == "NewState")
