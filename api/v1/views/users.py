2db39440fda56460757b9679c6fbc25c318e86bc
Ken Wekesa <wanyamak884@gmail.com>
2025-01-25 23:57:32 +0300

    user view coded

---
+++ api/v1/views/users.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""view for User object that handles all defaut RESTFul API actions"""
+from models import storage
+from models.user import User
+from api.v1.views import app_views
+from flask import jsonify
+from flask import make_response
+from flask import request
+
+
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def getUsers():
+    """retrieves the list of all User objects"""
+    all_users = storage.all(User)
+    return [val.to_dict() for val in all_users.values()]
+
+
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def getUser(user_id):
+    """retrieve a User object"""
+    all_users = storage.all(User)
+    for val in all_users.values():
+        if val.id == user_id:
+            return jsonify(val.to_dict())
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
+def deleteUser(user_id):
+    """delete a User object"""
+    all_users = storage.all(User)
+    for val in all_users.values():
+        if val.id == user_id:
+            storage.delete(val)
+            storage.save()
+            return make_response(jsonify({}), 200)
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def createUser():
+    """create a User"""
+    all_users = storage.all(User)
+    try:
+        data = request.get_json()
+        if "email" not in data:
+            return make_response("Missing email", 400)
+        if "password" not in data:
+            return make_response("Missing password", 400)
+        new_user = User()
+        for key, value in data.items():
+            setattr(new_user, key, value)
+        storage.new(new_user)
+        storage.save()
+        return make_response(jsonify(new_user.to_dict()), 201)
+    except Exception as e:
+        return make_response("Not a JSON", 400)
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def updateUser(user_id):
+    """update a User object"""
+    all_users = storage.all()
+    try:
+        data = request.get_json()
+        for val in all_users.values():
+            if val.id == user_id:
+                for k, v in data.items():
+                    setattr(val, k, v)
+                return make_response(jsonify(val.to_dict()), 200)
+    except Exception as e:
+        return make_response("Not a JSON", 400)
--- main.py
+++ main.py
@@ -5,14 +5,23 @@ import json
 import requests
 
 if __name__ == "__main__":
+    """ Get one amenity
+    """
     r = requests.get("http://0.0.0.0:5000/api/v1/amenities")
     r_j = r.json()
-    print(type(r_j))
-    print(len(r_j))
+    amenity_id = r_j[0].get('id')
+
+    """ DELETE /api/v1/amenities/<amenity_id>
+    """
+    r = requests.delete("http://0.0.0.0:5000/api/v1/amenities/{}".format(amenity_id))
+    print(r.status_code)
+    
+    """ Verify if the deleted amenity is not present anymore
+    """
+    r = requests.get("http://0.0.0.0:5000/api/v1/amenities")
+    r_j = r.json()
     for amenity_j in r_j:
-        if amenity_j.get('name') in ["Wifi", "Ethernet", "Soap", "Bed"]:
-            print("OK")
+        if amenity_j.get('id') == amenity_id:
+            print("amenitie is not deleted")
         else:
-            print("Missing: {}".format(amenity_j.get('name')))
-        if amenity_j.get('id') is None:
-            print("Missing ID for Amenity: {}".format(amenity_j.get('name')))
+            print("OK")
