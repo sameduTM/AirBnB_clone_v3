ce31071af864fc3774a0019c2616395fc3b2a4bd
Ken Wekesa <wanyamak884@gmail.com>
2025-01-30 19:52:17 +0300

    add delete method

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -39,3 +39,21 @@ def createPlaceAmenity(place_id, amenity_id):
     storage.save()
 
     return make_response(jsonify(amenity.to_dict()), 201)
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+    methods=['DELETE'], strict_slashes=False)
+def deletePlaceAmenity(place_id, amenity_id):
+    """delete Amenity object to a Place"""
+    place = storage.get(Place, place_id)
+    amenity = storage.get(Amenity, amenity_id)
+
+    if not place or not amenity:
+        return make_response(jsonify({"error": "Not found"}), 404)
+    if amenity not in place.amenities:
+        return make_response(jsonify({"error": "Not found"}), 404)
+
+    place.amenities.remove(amenity)
+    storage.save()
+
+    return make_response(jsonify({}), 200)
